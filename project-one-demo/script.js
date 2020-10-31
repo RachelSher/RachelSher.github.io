f4525481a902d9d593bb8973cee933470f0dbaaf
RachelSher <72037990+RachelSher@users.noreply.github.com>
2020-10-31 00:49:27 -0700

    Finished project one site

---
+++ project-one-demo/script.js
@@ -0,0 +1,13 @@
+
+      $(".photo1").click(function () {
+        $(".photo1-content").toggleClass("hide")
+        $(".photo1").toggleClass("bigger")
+      })
+      $(".photo2").click(function () {
+        $(".photo2-content").toggleClass("hide")
+        $(".photo2").toggleClass("bigger")
+      })
+      $(".photo3").click(function () {
+        $(".photo3-content").toggleClass("hide")
+        $(".photo3").toggleClass("bigger")
+      })
